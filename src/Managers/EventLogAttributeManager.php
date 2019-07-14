6c8fa98f0a9f6665d7f7f48bc80f4bfb780fb6c5
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/EventLogAttributeManager.php
+++ src/Managers/EventLogAttributeManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\EventLogAttribute newEntity()
+ * @method \Amethyst\Schemas\EventLogAttributeSchema getSchema()
+ * @method \Amethyst\Repositories\EventLogAttributeRepository getRepository()
+ * @method \Amethyst\Serializers\EventLogAttributeSerializer getSerializer()
+ * @method \Amethyst\Validators\EventLogAttributeValidator getValidator()
+ * @method \Amethyst\Authorizers\EventLogAttributeAuthorizer getAuthorizer()
+ */
 class EventLogAttributeManager extends Manager
 {
     use ConfigurableManager;
--- src/Managers/EventLogManager.php
+++ src/Managers/EventLogManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\EventLog newEntity()
+ * @method \Amethyst\Schemas\EventLogSchema getSchema()
+ * @method \Amethyst\Repositories\EventLogRepository getRepository()
+ * @method \Amethyst\Serializers\EventLogSerializer getSerializer()
+ * @method \Amethyst\Validators\EventLogValidator getValidator()
+ * @method \Amethyst\Authorizers\EventLogAuthorizer getAuthorizer()
+ */
 class EventLogManager extends Manager
 {
     use ConfigurableManager;
