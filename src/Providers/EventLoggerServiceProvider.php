d07465cdef304899846c4097624f2e7e60a3c627
railken <railken@protonmail.ch>
2020-02-01 19:33:44 +0100

    fix style

--- src/Providers/EventLoggerServiceProvider.php
+++ src/Providers/EventLoggerServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace Amethyst\Providers;
 
-use Amethyst\Core\Support\Router;
 use Amethyst\Core\Providers\CommonServiceProvider;
+use Amethyst\Core\Support\Router;
 use Amethyst\Managers\EventLogAttributeManager;
 use Amethyst\Managers\EventLogManager;
 use Doctrine\Common\Inflector\Inflector;
